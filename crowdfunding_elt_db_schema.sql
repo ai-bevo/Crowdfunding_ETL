c302e39d3541e0aea603cac24bc58f53a896d7a4
ai-bevo <allan.ivey@gmail.com>
2023-05-25 21:26:24 -0500

    schema file

--- crowdfunding_elt_db_schema.sql
+++ crowdfunding_elt_db_schema.sql
@@ -1,47 +1,47 @@
 DROP TABLE category;
 CREATE TABLE category(
 	category_id VARCHAR NOT NULL PRIMARY KEY,
-	category VARCHAR NOT NULL
+	category VARCHAR NOT NULL,
+	FOREIGN KEY (category_id) REFERENCES campaign(category_id)
+	
 );
 SELECT * FROM category;
 
 DROP TABLE subcategory;
 CREATE TABLE subcategory(
 	subcategory_id VARCHAR NOT NULL PRIMARY KEY,
-	subcategory VARCHAR NOT NULL
+	subcategory VARCHAR NOT NULL,
+	FOREIGN KEY (subcategory_id) REFERENCES campaign(subcategory_id)
 );
 SELECT * FROM subcategory;
 
 DROP TABLE campaign;
 CREATE TABLE campaign(
 	cf_id INT NOT NULL PRIMARY KEY,
-	contact_id VARCHAR NOT NULL,
+	contact_id INT NOT NULL PRIMARY KEY,
 	company_name VARCHAR NOT NULL,
 	description VARCHAR NOT NULL,
-	goal VARCHAR NOT NULL,
-	pledged VARCHAR NOT NULL,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
 	outcome VARCHAR NOT NULL,
-	backers_count VARCHAR NOT NULL,
+	backers_count INT NOT NULL,
 	country VARCHAR NOT NULL,
 	currency VARCHAR NOT NULL,
 	launch_date VARCHAR NOT NULL,
 	end_date VARCHAR NOT NULL,
 	category_id VARCHAR NOT NULL,
-	subcategory_id VARCHAR NOT NULL,
-	FOREIGN KEY () REFERENCES titles()
+	subcategory_id VARCHAR NOT NULL
 );
 SELECT * FROM campaign;
 
 
 DROP TABLE contacts;
 CREATE TABLE contacts(
-	contact_id INT NOT NULL,
+	contact_id INT NOT NULL PRIMARY KEY
 	first_name VARCHAR NOT NULL,
-	last_name
-	email
-	PRIMARY KEY (, ),
-	FOREIGN KEY () REFERENCES (),
-	FOREIGN KEY () REFERENCES ()
+	last_name VARCHAR NOT NULL,
+	email VARCHAR NOT NULL
+	FOREIGN KEY(contact_id) REFERENCES campaign(contact_id),
 );
 SELECT * FROM contacts;
 
